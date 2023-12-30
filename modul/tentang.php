0fedbf2f0d0102f9a4d216e7256a57d82f0b4a31
exo12dyorama <44akmalakmal@students.unnes.ac.id>
2023-12-29 19:50:05 -0800

    first commit

---
+++ modul/tentang.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+   <title>Tentang - Chirexs 1.0</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='aset/tentang/style.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+   <link rel="stylesheet" href="aset/login/css/style.css">
+  
+  
+</head>
+<style>
+@media only screen and (max-width: 400px) {
+    img {
+        width: 100%;
+    }
+}
+</style>
+<body>
+
+  <div class="ma3">
+  <article class="tc w-75 center pt5 pb2 ph3 mw6-ns ba bw1 b--light-gray" style="background: #fff;">
+    <header class="mb4">
+		<img class="br-100" src="gambar/admin/kebow.jpg" alt="Profile headshot" />
+      <h1 class="f3 lh-title mv2 dark-gray">Kebodeteksi 1.0</h1>
+	  <p class="f6 silver mt2 mb0"><a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pengembang Aplikasi"><i class="fa fa-user" aria-hidden="true"></i> Akmal</button></a>
+<a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pakar Aplikasi"><i class="fa fa-user-md" aria-hidden="true"></i> Muhammad Akmal Dzakwan</button></a>
+	 <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Dosen Pembimbing"><i class="fa fa-user-plus" aria-hidden="true"></i> Prof. Dr. Subiyanto S.T., M.T.</button></a></p>
+	   <br>
+	   <h2 class="f5 silver mt2 mb1">Sistem Pakar, Diagnosa Penyakit Pada Kerbau</h2>
+	  <h2 class="f5 silver mt2 mb1">Copyright © 2023, <a class="link dim silver">Universitas Negeri Semarang</a></h2>
+	  <br>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-chrome" aria-hidden="true"></i></a>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-firefox" aria-hidden="true"></i></a>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-edge" aria-hidden="true"></i></a>
+	  <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-safari" aria-hidden="true"></i></a>
+	  <a class="link dim light-silver"  style="font-size: 40px;"> | </a>
+	       <a class="link dim light-silver"  style="font-size: 40px;"> 	<img src="aset/tentang/unnes-logo.png" alt="Profile headshot" /></a>
+    </header>
+    <p class="f6 tl lh-copy silver" style="margin: 20px;">Sistem pakar yang mampu mendiagnosa penyakit pada Kerbau berdasarkan pengetahuan yang diberikan langsung dari pakar/ahlinya dan melalui studi  literatur. Penelitian ini menggunakan metode perhitungan Certainty Factor (CF) dalam menghitung tingkat kepakaran. Data penelitian ini terdiri dari data gejala dan data penyakit Kerbau, serta data aturan. Sistem pakar pada organisasi ditujukan  untuk penambahan  value, peningkatan produktivitas serta area manajerial yang dapat mengambil kesimpulan dengan cepat.</p>
+  </article>
+</div>
+</body>
+
+</html>
